bb8f025d5e86c3ef7bdb30e6067c682b0c8cad65
Ryanethier <rethier@luc.edu>
2025-07-14 14:25:56 +0000

    Created add() method, created printTable() method

--- HashTable.java
+++ HashTable.java
@@ -30,14 +30,69 @@ public class HashTable<E extends Comparable<E>> {
         this(DEFAULT_SIZE);
     }
 
+
     /**
-     * Adds a node, with the specified content, to a linked list in the underlying
-     * array.
+     * Adds a new element to the hash table.
+     * Computes the hash index from the element's hashCode,
+     * then inserts the element at the "head" of the linked list at that index.
+     * Duplicate elements ( by .equals()) are ignored.
      * 
      * @param content E The content of a new node, to be placed in the array.
      */
+
+
     public void add(E content) {
-        // COMPLETE THIS METHOD
-    } // method add
+
+        // Calculate hash index (using .abs to make sure non-negative)
+        int index = Math.abs(content.hashCode() % this.underlying.length); 
+
+        // Start at the head of linked list at given index
+
+        Node<E> currentNode = underlying[index];
+
+        // Check if element already exists in the list (not allowing for duplicates)
+
+        while (currentNode != null) {
+            if (currentNode.getContent().equals(content)) {
+                return; // Element already exists, do not insert again
+            }
+
+            currentNode = currentNode.getNext();
+            
+        }
+
+        // Create a new node and insert at the head of the list
+
+        Node<E> newNode = new Node<E>(content);
+        newNode.setNext(this.underlying[index]);
+        underlying[index] = newNode;
+        
+        
+      } // method add
+
+      /**
+       *  Method to print the contents of the HashMap
+       *  If the currentNode is not null, the method iterates through the array
+       *  and prints the index followed by its contents (linked-list)
+       *  Else, the method prints null to indicate no contents at specified index.
+       * 
+       * EXAMPLE: Index 0 -> apple -> banana -> null
+       * 
+       */
+
+      public void printTable() {
+        for (int i = 0; i < this.underlying.length; i++) {
+            System.out.println("Index " + i + " -> ");
+            Node<E> currentNode = underlying[i];
+            while (currentNode != null) {
+
+                System.out.print(currentNode + " -> ");
+                currentNode = currentNode.getNext();
+                
+            }
+
+            System.out.println("null");
+        }
+      }
 
 } // class HashTable
